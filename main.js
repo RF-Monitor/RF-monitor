e4a0516e1f1170905af8ed6e16666e972161300e
unknown <mcred0227@gmail.com>
2024-09-03 01:06:55 +0800

    New config backup and replace function

--- main.js
+++ main.js
@@ -21,11 +21,29 @@ let win =  null;
     });
   }, 5000);
 });*/
+
+/*----------創建config資料夾----------*/
 if (!fs.existsSync(path.join(__dirname, '../../../../RF-Monitor_config/'))){
   fs.mkdirSync(path.join(__dirname, '../../../../RF-Monitor_config/'));
 }
+/*----------指定config資料夾----------*/
 storage.setStoragePath(path.join(__dirname, '../../../../RF-Monitor_config/config.json'));
 console.log(storage.getStoragePath());
+
+console.log(storage.getItem('server_url'));
+
+/*----------config檔案損毀處理----------*/
+const fileAPath = path.join(__dirname, '../../../../RF-Monitor_config/config.json');
+const fileBPath = path.join(__dirname, '../../../../RF-Monitor_config/config_backup.json');
+if (storage.getItem('server_url') === null) {
+  console.log("config.json is broken,using backup file.")
+  // 删除文件A
+  fs.unlinkSync(fileAPath);
+  console.log('File A deleted successfully.');
+  // 重命名文件B为config.json
+  fs.renameSync(fileBPath, fileAPath);
+  console.log("Backup config file successfully used.")
+}
 
 if(storage.getItem('server_url')  == ""){
   storage.setItem('server_url','http://RFEQSERVER.myqnapcloud.com')
@@ -196,6 +214,15 @@ if(storage.getItem('Canvas3Sta')  == ""){
 if(storage.getItem('Canvas4Sta')  == ""){
   storage.setItem('Canvas4Sta',"6050_0003")
 }
+
+/*----------備份config.json----------*/
+const sourcePath = path.join(__dirname, '../../../../RF-Monitor_config/config.json');
+const destinationPath = path.join(__dirname, '../../../../RF-Monitor_config/config_backup.json');
+
+fs.copyFileSync(sourcePath, destinationPath);
+console.log('File copied to config_backup.json successfully.');
+
+
 const createWindow = () => {
     win = new BrowserWindow({
       width: 1250,
