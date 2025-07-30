f2edb1fcb1656581782148d32ec71397f8a96daf
unknown <mcred0227@gmail.com>
2025-07-30 20:26:42 +0800

    2.7.0-pre8

--- js/main/tsunami.js
+++ js/main/tsunami.js
@@ -28,14 +28,23 @@
                     document.getElementById("tsunami_epicenter").innerHTML = data["epicenter_location"].replace("　","<br>");
                     document.getElementById("tsunami_magnitude").innerHTML = data["magnitude"];
 
+                    // 警報顏色
+                    const tsunami_color_c = data["report_color"];
+                    const tsunami_color_E = tsunami_color[tsunami_color_c];
+                    document.getElementById("tsunami_color").style.backgroundColor = tsunami_color_E;
+
+                    //字體顏色
+                    if(tsunami_color_E == "yellow"){
+                        document.getElementById("tsunami_type_color").style.color = "black";
+                    }else{
+                        document.getElementById("tsunami_type_color").style.color = "white";
+                    }
+                        
+                    //海嘯警報/警訊/消息/解除
+                    document.getElementById("tsunami_type").innerHTML = data["report_type"];
+
                     // 警報類型
                     if(data["report_type"] == "海嘯警報"){
-                        const tsunami_color_c = data["report_color"];
-                        const tsunami_color_E = tsunami_color[tsunami_color_c];
-                        document.getElementById("tsunami_color").style.backgroundColor = tsunami_color_E;
-                        document.getElementById("tsunami_type_color").style.color = "white";
-                        document.getElementById("tsunami_type").innerHTML = data["report_type"];
-
                         // 預測波高
                         for(let i = 0 ;i < data["forecast"].length ;i++){
                             let area_c = data["forecast"][i]["area_name"]
@@ -51,12 +60,9 @@
                         }
                         playAudio_eew(['./audio/tw/tsunami/tsunami_warning.mp3','./audio/tw/tsunami/tsunami_warning_released.mp3']);
                     }else if(data["report_type"] == "海嘯警訊"){
-                        document.getElementById("tsunami_color").style.backgroundColor = tsunami_color_E;
-                        document.getElementById("tsunami_type_color").style.color = "white";
-
-                        document.getElementById("tsunami_type").innerHTML = data["report_type"]
-
+                        // 預測波高
                         for(let i = 0 ;i < data["forecast"].length ;i++){
+                            
                             let area_c = data["forecast"][i]["area_name"]
                             let area = tsunami_coastline[area_c];
                             document.getElementById(area).innerHTML = data["forecast"][i]["wave_height"];
@@ -64,17 +70,16 @@
                             let color_c = data["forecast"][i]["area_color"];
                             let color = tsunami_color[color_c];
                             document.getElementById(area+"_color").style.borderColor = color;
+                        }
+                        if(data["forecast"].length == 0){
+                            document.getElementById("tsunami_forecast").style.display = "none";
                         }
                         if(data["forecast"].length == 0){
                             document.getElementById("tsunami_forecast").style.display = "none";
                         }
                         playAudio_eew(['./audio/tw/tsunami/tsunami_warning.mp3','./audio/tw/tsunami/tsunami_released.mp3']);
                     }else if(data["report_type"] == "海嘯消息"){
-                        document.getElementById("tsunami_color").style.backgroundColor = tsunami_color_E;
-                        document.getElementById("tsunami_type_color").style.color = "black";
-
-                        document.getElementById("tsunami_type").innerHTML = data["report_type"]
-
+                        // 預測波高
                         for(let i = 0 ;i < data["forecast"].length ;i++){
                             let area_c = data["forecast"][i]["area_name"]
                             let area = tsunami_coastline[area_c];
@@ -87,15 +92,13 @@
                         if(data["forecast"].length == 0){
                             document.getElementById("tsunami_forecast").style.display = "none";
                         }
-                        playAudio_eew(['./audio/tw/tsunami/tsunami_info_released.mp3']);
-                    }else if(data["report_type"] == "海嘯警報解除" || data["report_type"] == "海嘯報告"){
-                        document.getElementById("tsunami_color").style.backgroundColor = tsunami_color_E;
-                        document.getElementById("tsunami_type_color").style.color = "white";
-
-                        document.getElementById("tsunami_type").innerHTML = data["report_type"]
                         if(data["forecast"].length == 0){
                             document.getElementById("tsunami_forecast").style.display = "none";
                         }
+                        playAudio_eew(['./audio/tw/tsunami/tsunami_info_released.mp3']);
+                    }else if(data["report_type"] == "海嘯警報解除" || data["report_type"] == "海嘯報告"){
+                        document.getElementById("tsunami_forecast").style.display = "none";
+
                         playAudio_eew(['./audio/tw/tsunami/tsunami_cleared.mp3']);
                     }
 
@@ -108,7 +111,7 @@
                     if(tsunami_timer != null){
                         clearTimeout(tsunami_timer);
                     }
-                    tsunami_timer = setTimeout(tsunamiTimeout,86400000);
+                    tsunami_timer = setTimeout(tsunamiTimeout,86400000 - (timestampNow() - created_at));
                 }else{
                     document.getElementById("tsunami").style.display = "none"
                 }
