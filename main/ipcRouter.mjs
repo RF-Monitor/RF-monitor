ce3bfeef625d1e582c994824bbb289e4f79fa3a9
Rexi0227 <mcred0227@gmail.com>
2026-01-02 00:04:42 +0800

    cancel console.log() when broadcasting

--- main/ipcRouter.mjs
+++ main/ipcRouter.mjs
@@ -3,7 +3,7 @@ import { BrowserWindow } from 'electron';
 export function broadcastEvent(channel, payload) {
   BrowserWindow.getAllWindows().forEach(win =>{
     win.webContents.send(channel, payload)
-    console.log("[ipcbroadcast]sending via",channel)
+    //console.log("[ipcbroadcast]sending via",channel)
   });
 }
 
