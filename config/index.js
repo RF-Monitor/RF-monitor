63f01af6a42aea1e8a1fbb12a9f8ff4f9290f90d
Rexi0227 <mcred0227@gmail.com>
2026-01-02 00:04:07 +0800

    make backupConfig as a function, remake mkdir

--- config/index.js
+++ config/index.js
@@ -2,16 +2,23 @@ const storage = require('electron-localstorage');
 const fs = require('fs');
 const path = require('path');
 const defaults = require('./defaults');
+const config = require("./config");
+const configFolderPath = path.join(__dirname, '../../../../RF-Monitor_config/')
+const configFilePath = path.join(__dirname, '../../../../RF-Monitor_config/config.json')
 
 /*----------創建config資料夾----------*/
-if (!fs.existsSync(path.join(__dirname, '../../../../RF-Monitor_config/'))){
-  fs.mkdirSync(path.join(__dirname, '../../../../RF-Monitor_config/'));
+if (!fs.existsSync(configFolderPath)){
+  fs.mkdirSync(configFolderPath);
 }
+/*----------創建config檔案----------*/
+/*
+if (!fs.existsSync(configFilePath)) {
+    fs.writeFileSync(configFilePath, "{}", "utf8");
+}*/
 /*----------指定config資料夾----------*/
-storage.setStoragePath(path.join(__dirname, '../../../../RF-Monitor_config/config.json'));
+storage.setStoragePath(configFilePath);
 console.log(storage.getStoragePath());
-
-console.log(storage.getItem('server_url'));
+console.log("server url:",storage.getItem('server_url'));
 
 /*----------config檔案損毀處理----------*/
 function repairIfBroken() {
@@ -31,17 +38,19 @@ function repairIfBroken() {
 function applyDefaults(){
     defaults.applyDefaults()
 }
+function backupConfig() {
+    /*----------備份config.json----------*/
+    const sourcePath = path.join(__dirname, '../../../../RF-Monitor_config/config.json');
+    const destinationPath = path.join(__dirname, '../../../../RF-Monitor_config/config_backup.json');
 
-/*----------備份config.json----------*/
-const sourcePath = path.join(__dirname, '../../../../RF-Monitor_config/config.json');
-const destinationPath = path.join(__dirname, '../../../../RF-Monitor_config/config_backup.json');
-
-fs.copyFileSync(sourcePath, destinationPath);
-console.log('File copied to config_backup.json successfully.');
-
+    fs.copyFileSync(sourcePath, destinationPath);
+    console.log('File copied to config_backup.json successfully.');
+}
 module.exports = {
     applyDefaults,
     repairIfBroken,
+    backupConfig,
     get: storage.getItem,
     set: storage.setItem,
+    config: config.getConfig
 };
